1c085523bd8621ff3e0f0cf9a8ffdbbb72c0c784
kellenklein <kellenjklein@gmail.com>
2026-02-16 21:13:47 -0600

    Updated sound file to compile-time inclusion

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::fs::File;
+use std::io::Cursor;
 use std::num::NonZeroU16;
 use std::ops::RangeInclusive;
 use rodio::{Decoder, source::{Source, LimitSettings}, cpal::BufferSize};
@@ -28,18 +28,6 @@ fn max100(input: &str) -> Result<u8, String> {
     }
 }
 
-// A function to parse valid audio files to play as the metronome
-fn valid_path(input: &str) -> Result<String, String> {
-    match File::open(input) {
-        Ok(file) => {
-            Decoder::try_from(file)
-                .map(|_| format!("{}", input))
-                .map_err( |err| format!("{}", err) )
-        },
-        Err(err) => { Err(format!("{}", err)) }
-    }
-}
-
 // Derive from parser to allow direct arg processing
 #[derive(Parser)]
 #[command(name = "tempo")]
@@ -50,10 +38,6 @@ struct Cli {
     // The beats per minute of the metronome (default: 100bpm)
     #[arg(short, long, default_value_t = NonZeroU16::new(100).unwrap())]
     bpm: NonZeroU16,
-    // Path to audio file to play as metronome (default: audio/sound.wav)
-    // Sourced royalty free as Metronome SFX by P4WZ on Sample Focus
-    #[arg(short, long, default_value_t = String::from("audio/sound.wav"), value_parser = valid_path)]
-    file: String, 
     // Volume from 0 to 100
     #[arg(short, long, default_value_t = 5, value_parser = max100)]
     volume: u8,
@@ -74,9 +58,9 @@ struct Cli {
         .open_stream()
         .expect("open default audio stream");
 
-    // Load a sound from the provided argument file, path relative to Cargo.toml
-    let file = File::open(&metronome.file)
-        .expect("cli arguments parsed");
+    // Load the default sound file, using macro include_bytes! for compile-time inclusion
+    let file_bytes: &'static [u8] = include_bytes!("audio/sound.wav");
+    let cursor = Cursor::new(file_bytes);
    
     // Create sound limiting settings so the noise isn't harsh
     let settings = LimitSettings::new()
@@ -86,30 +70,39 @@ struct Cli {
         .with_release(Duration::from_micros(50));
     
     // Decode the sound file into a source, limiting the volume and sound peaks
-    let source = Decoder::try_from(file)
+    let source = Decoder::try_from(cursor)
         .expect("input file is valid")
         .amplify(0.1 * f32::from(metronome.volume))
         .limit(settings);    
 
     // Create a buffered version of the source to repeatedly pull from
     let buffer = source.buffered();
-   
 
+    // Asynchronous runtime to repeat bpm counter
     let rt = tokio::runtime::Runtime::new().unwrap();
+    // Blocking (only task)
     rt.block_on(async {
+        // Create a visual bpm indicator inside the async runtime
         let indicator = ProgressBar::with_draw_target(Some(1), ProgressDrawTarget::stdout_with_hz(255));
         indicator.set_prefix(format!("{} bpm", metronome.bpm));
         indicator.set_message("Ctrl+C to exit");
         indicator.set_style(ProgressStyle::with_template("{prefix:} {bar:^.white/black} {msg:}").unwrap());
+        // Designate the time interval of the metronome
         let mut interval = time::interval(time_per_beat);
+        // Move the indicator from 0% to 100%
         indicator.inc(1);
         loop {
+            // Clone the metronome sound that we're repeating
             let sound = buffer.clone();
+            // Sleep a small period of time to allow the indicator to flicker
             time::sleep(interval.period().div_f32(20.0)).await;
+            // Move the indicator back to 0%
             indicator.dec(1);
+            // Now we await the time interval (previous operations should take less than 1 interval)
             interval.tick().await;
+            // Add the cloned sound to the buffer to play it
             stream_handle.mixer().add(sound);
-
+            // Now we can move the indicator back to 100% while the sound plays
             indicator.inc(1);
         }
     });
